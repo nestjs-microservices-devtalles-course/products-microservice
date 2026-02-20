533a302ab0d5d1314cfa7e3b9db628b4a195b025
Luciano Figueroa <lumusitech@gmail.com>
2026-02-20 16:15:28 -0300

    chore: add validation for envs using joi schema

---
+++ src/config/envs.ts
@@ -0,0 +1,30 @@
+import * as joi from 'joi';
+
+process.loadEnvFile();
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envVarsSchema = joi
+  .object<EnvVars>({
+    PORT: joi.number().default(3000),
+  })
+  .unknown(true);
+
+function validateSchema<T>(
+  schema: joi.ObjectSchema<T>,
+  input: Record<string, unknown>,
+): T {
+  const result = schema.validate(input, { abortEarly: false });
+
+  if (result.error) {
+    throw new Error(`Config validation error: ${result.error.message}`);
+  }
+
+  return result.value;
+}
+
+const rawEnv: Record<string, unknown> = process.env;
+
+export const envs: EnvVars = validateSchema(envVarsSchema, rawEnv);
