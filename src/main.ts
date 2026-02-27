50061c2edd92ec2c830eb6749661e89a04027b19
Luciano Figueroa <lumusitech@gmail.com>
2026-02-27 20:33:59 -0300

    feat: update api rest products to products microservice

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.PORT,
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +20,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.PORT);
+  await app.listen();
 
   logger.log(`Products Microservice is running on port ${envs.PORT}`);
 }
